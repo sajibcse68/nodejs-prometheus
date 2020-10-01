7030cd5b0063e30efbc08a152dfd4c47830a5af0
sajibcse68 <sajibcse68@gmail.com>
2020-10-02 04:02:56 +0600

    Exposing default metrics

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const url = require('url')
+const client = require('prom-client')
+
+// Create a Registry which registers the metrics
+const register = new client.Registry()
+
+// Add a default label which is added to all metrics
+register.setDefaultLabels({
+  app: 'example-nodejs-app'
+})
+
+// Enable the collection of default metrics
+client.collectDefaultMetrics({ register })
+
+// Define the HTTP server
+const server = http.createServer(async (req, res) => {
+  // Retrieve route from request object
+  const route = url.parse(req.url).pathname
+
+  if (route === '/metrics') {
+    // Return all metrics the Prometheus exposition format
+    res.setHeader('Content-Type', register.contentType)
+    res.end(register.metrics())
+  }
+})
+
+// Start the HTTP server which exposes the metrics on http://localhost:8080/metrics
+server.listen(8080)
